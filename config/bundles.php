8344b2ecbcfd529ff561f32cdd193ee729b0374c
Louis Roger Guirika <louisrogerguirika@gmail.com>
2020-11-26 08:18:13 +0000

    Ajout de la fixtures du produit et de Webpack(non utilisé

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
